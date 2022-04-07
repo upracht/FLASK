e3d9a415e675443d7b39612010a5555a4984a838
upracht <81009637+upracht@users.noreply.github.com>
2022-04-07 12:34:56 +0200

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -10,7 +10,7 @@ yes | apt install python3-pip
 yes | apt install  libatlas-base-dev
 
 pip3 install numpy
-pip3 install pillow==9.0.1
+#pip3 install pillow==9.0.1
 pip3 install matplotlib
 pip3 install scipy
 pip3 install pi-ina219
